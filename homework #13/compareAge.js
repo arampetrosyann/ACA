bd289100a455df86425f55e63263c717e4739690
arampetrosyan <0aramp@gmail.com>
2020-06-25 22:06:09 +0400

    create a method which compare persons ages

--- homework #13/compareAge.js
+++ homework #13/compareAge.js
@@ -1,30 +1,28 @@
 "use strict"
 
 class Person {
-    constructor(name, age) {
-		this.name = name;
-		this.age = age;
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  compareAge(other) {
+    let sentence = "";
+
+    if (this.age < other.age) {
+      sentence = `${other.name} is older than me.`;
+    } else if (this.age > other.age) {
+      sentence = `${other.name} is younger than me.`;
+    } else {
+      sentence = `${other.name} is the same age as me.`;
     }
 
-	compareAge(other) {
-        let sentence = "";
+    return sentence;
+  }
 
-		if (this.age < other.age) {
-            sentence = `${other.name} is older than me.`;
-
-        } else if (this.age > other.age) {
-            sentence = `${other.name} is younger than me.`;
-
-        } else {
-            sentence = `${other.name} is the same age as me.`;
-        }	
-
-        return sentence;
-    }
-    
-    toString() {
-        return `Person: ${this.name}`;
-    }
+  toString() {
+    return `Person: ${this.name}`;
+  }
 };
 
 const michael = new Person("Michael", 51);
